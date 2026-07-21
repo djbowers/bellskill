10c526bcd5de9720ab79b39a241273101121df7e
DJ Bowers <daniel_bowers@icloud.com>
2026-07-21 11:58:41 -0500

    chore(storybook): upgrade to 8.6.18 (#153)
    Bump all @storybook/* packages from 7.2.3 to 8.6.18 and align
    eslint-plugin-storybook to 0.12.0 (last SB8-compatible line).
    
    Drop three packages that were dead weight: @storybook/testing-react
    and @storybook/testing-library (imported nowhere; tests use
    composeStories from @storybook/react) and @storybook/addon-styling
    (listed in main.ts but wired no decorator).

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
   ],
   framework: {
     name: '@storybook/react-vite',
